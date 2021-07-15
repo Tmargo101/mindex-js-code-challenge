560a9d8d9144cd685afe968a7b6979ae45b1ad4a
Thomas Margosian <tmargo101@gmail.com>
2021-07-15 13:23:22 -0400

    Task 2 Complete. - Added method to employee component to recursively get all reports for an employee - Changed card header to display report information if the employee has any reports.

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,43 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 import {Employee} from '../employee';
+import {EmployeeService} from '../employee.service';
+
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
   @Input() employee: Employee;
+  totalReports: number;
 
-  constructor() {
+  constructor(private employeeService: EmployeeService) {
+    this.totalReports = 0;
+  }
+
+  ngOnInit(): void {
+    
+    // Get total reports (direct and indirect)
+    this.getReports(this.employee);
+  }
+
+  // Recursivly get the total number of reports, directly and indirectly
+  private getReports(employee: Employee): void {
+
+    // Return if no direct reports of the passed employee are found
+    if (!employee.directReports) { return; }
+
+    // Add all direct reports of the currently passed employee
+    this.totalReports += employee.directReports.length;
+
+    // Recursivly check for indirect reports
+    employee.directReports.forEach(e => {
+      this.employeeService.get(e)
+        .subscribe(report => {
+          this.getReports(report);
+        });
+    });
   }
 }
